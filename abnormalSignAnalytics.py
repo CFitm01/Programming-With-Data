a925181a0d5d94771d1f7065898671f89a6a82d0
CFitm01 <56835338+CFitm01@users.noreply.github.com>
2019-12-11 14:39:38 +0000

    The Analytics as Dictionaries

--- abnormalSignAnalytics.py
+++ abnormalSignAnalytics.py
@@ -1,37 +1,70 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Fri Nov 29 14:35:32 2019
+
+@author: CFitz
+"""
+import matplotlib.pyplot as plt
+import numpy as num
+#We want to find the abnormal values for pulse or blood pressure.
+#The abnormal values for pulse is anything that  is below 60 or above 99.
+#The abnormal values for blood pressure is 121.
+#The pulse values are stored at [i][3] & [i][4] respectively.
+#Searching functions include for loops, 
+
+
+
 def abnormalSignAnalytics(array):
     Vital = input('Which Vital do you wish to search for?: ')
     Count = 0
-    Vital_Record = []
-    Abnormal_Record = []
-    graphNormal = []
-    graphAbnormal = []
+    Vital_Record = {}
+    Abnormal_Record = {}
+    Abnormal_Patients = []
     for i in range(1, len(array)-1):
         if Vital == 'Pulse':
             if array[i][3] < 60 or array[i][3] > 99:
-                Abnormal_Record.append([array[i][0], array[i][3]])
-                graphAbnormal.append(array[i][3])
                 Count += 1
+                if array[i][3] in Abnormal_Record:
+                    Abnormal_Record[array[i][3]] += 1
+                    Abnormal_Patients.append(i)
+                else:
+                    Abnormal_Record[array[i][3]] = 1
+                    Abnormal_Patients.append(i)
             else:
-                Vital_Record.append([array[i][0], array[i][3]])
-                graphNormal.append(array[i][3])
+                if array[i][3] in Vital_Record:
+                    Vital_Record[array[i][3]] += 1
+                else:
+                    Vital_Record[array[i][3]] = 1
         if Vital == 'Blood Pressure':
             if array[i][4] == 121:
-                Vital_Record.append([array[i][0], array[i][4]])
-                graphAbnormal.append(array[i][4])
-                Count += 1
+                if array[i][4] not in Abnormal_Record:
+                    Abnormal_Record[array[i][4]] = 1
+                    Abnormal_Patients.append(i)
+                    Count += 1
+                else:
+                    Abnormal_Record[array[i][4]] = 1
+                    Abnormal_Patients.append(i)
+                    Count += 1
             else:
-                Vital_Record.append([array[i][0], array[i][4]])
-                graphNormal.append(array[i][4])
-    print("There were {} abnormal {} values: ".format(Count, Vital) + \
-          str(Abnormal_Record))
+                if array[i][4] not in Vital_Record:
+                    Vital_Record[array[i][4]] = 1
+                else:
+                    Vital_Record[array[i][4]] += 1              
+                
+    print("There were {} abnormal {} values:".format(Count, Vital))
+    print("The following patients have abnormal {} values:".format(Vital) +\
+          str(Abnormal_Patients))
     Analyse = input('Do you wish to see these results in graphical form?: Y/N')
     if Analyse.upper() == 'Y':
-        return frequencyAnalytics(graphNormal, graphAbnormal)
+        return frequencyAnalytics(Vital_Record, Abnormal_Record)
     else:
         return Vital_Record
         
     
 
 def frequencyAnalytics(normalArray, abnormalArray):
-    plt.hist(abnormalArray, bins = [range(50, 100, 10)], rwidth = 0.95,\
-             color = 'orange')
+    x = normalArray.keys()
+    y = normalArray.values()
+    plt.hist(xy, bins = [i for i in range(50, 100, 10)])
+
+    
